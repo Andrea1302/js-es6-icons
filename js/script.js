aa238fc8e6bab5b5dd32da9e169b7f7e73a2fd6f
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-08 16:58:33 +0100

    Varie prove select

--- js/script.js
+++ js/script.js
@@ -138,3 +138,97 @@ icone.forEach((oggetto)=> {
     mainContainer.innerHTML += innerHtml
 })
 
+
+
+
+
+// // prova filtra per tipo 
+// var selezione = document.getElementById("selezione_icone");
+// function show(){
+// //   var as = document.forms[0].ddlViewBy.value;
+//   var strUser = selezione.value;
+//   console.log(strUser);
+// //   console.log(as, strUser);
+// }
+// selezione.onchange=show;
+// show();
+
+// const user = icone.filter((elemento)=> elemento.type === "user");
+// console.log(user);
+
+// const vegetable = icone.filter((elemento)=> elemento.type === "vegetable")
+// console.log(vegetable);
+
+// const animal = icone.filter((elemento)=> elemento.type === "animal")
+// console.log(animal);
+
+// var selezione = document.getElementById("selezione_icone");
+
+// // function show(){
+// //   var as = document.forms[0].ddlViewBy.value;
+// var elementoOggetto = selezione.value;
+
+// user.map((elemento)=>{
+//     if ( elementoOggetto === elemento.type ){
+//         // console.log(elemento.name);
+//         let innerHtml = `
+//         <button class="icone_struttura"> 
+//              <i class="${elemento.family} ${elemento.prefix}${elemento.name} ${elemento.color}"></i>
+//              <div> ${elemento.name} </div>
+//          </button>`
+//         return innerHtml
+//       } else {
+//         icone.map((oggetto)=> {
+//             // console.log(oggetto.name);
+//             innerHtml = `
+//             <button class="icone_struttura"> 
+//                  <i class="${oggetto.family} ${oggetto.prefix}${oggetto.name} ${oggetto.color}"></i>
+//                  <div> ${oggetto.name} </div>
+//              </button>`
+//             // mainContainer.innerHTML += innerHtml
+//             return innerHTML
+//         })
+//       }
+// })
+// animal.map((elemento)=>{
+//     if ( elementoOggetto === elemento.type ){
+//         let innerHtml = `
+//         <button class="icone_struttura"> 
+//              <i class="${elemento.family} ${elemento.prefix}${elemento.name} ${elemento.color}"></i>
+//              <div> ${elemento.name} </div>
+//          </button>`
+//         return innerHtml
+//       } else {
+//         icone.map((oggetto)=> {
+//             // console.log(oggetto.name);
+//             innerHtml = `
+//             <button class="icone_struttura"> 
+//                  <i class="${oggetto.family} ${oggetto.prefix}${oggetto.name} ${oggetto.color}"></i>
+//                  <div> ${oggetto.name} </div>
+//              </button>`
+//             return innerHtml
+//         })
+//       }
+// })
+// vegetable.map((elemento)=>{
+//     if ( elementoOggetto === elemento.type ){
+//         let innerHtml = `
+//         <button class="icone_struttura"> 
+//              <i class="${elemento.family} ${elemento.prefix}${elemento.name} ${elemento.color}"></i>
+//              <div> ${elemento.name} </div>
+//          </button>`
+//          return innerHtml
+//       } else {
+//         icone.map((oggetto)=> {
+//             // console.log(oggetto.name);
+//             innerHtml = `
+//             <button class="icone_struttura"> 
+//                  <i class="${oggetto.family} ${oggetto.prefix}${oggetto.name} ${oggetto.color}"></i>
+//                  <div> ${oggetto.name} </div>
+//              </button>`
+//             return innerHtml
+//         })
+//       }
+// })
+// const inn = oggetto
+// mainContainer.innerHTML += inn
